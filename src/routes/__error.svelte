916e4c0aae92516f0dd66aae1ac1f591bdf45eb4
Sophie <me@soopy.moe>
2021-11-15 07:14:54 +0800

    fix: fixed broken 404 page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -17,9 +17,11 @@ export let content;
 </script>
 
 {#if status === 404}
-<div class="container ring-pink-200 mx-auto content-center">
-    <h1>404</h1>
-    <p>Page not found</p>
+<div class="container mx-auto px-4">
+    <h1 class="w-min mx-auto text-8xl text-pink-400">404</h1>
+    <p class="w-max mx-auto">Page not found.</p>
+    <br>
+    <p class="w-max mx-auto"><a href="/" class="rounded p-2 bg-opacity-25 bg-gray-300 hover:bg-opacity-75 hover:border-gray-700 hover:border">Return to home?</a></p>
 </div>
 {:else}
 <div>
