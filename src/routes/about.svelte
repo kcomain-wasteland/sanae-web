dcde1cfc68994c44e131ed3c4c1a9f771ebe18a8
Sophie <me@soopy.moe>
2021-11-18 00:33:50 +0800

    feat: about page

---
+++ src/routes/about.svelte
@@ -0,0 +1,27 @@
+<svelte:head>
+    <title>About - Sanae</title>
+</svelte:head>
+
+<div class="flex flex-col m-5 max-w-full">
+    <h1 class="text-5xl">About</h1>
+    <br class="my-2">
+
+    <h3 class="text-2xl">Acknowledgements</h3>
+    <p>The sources of the external assets used in this project are listed below.</p>
+    <ul class="list-disc list-inside">
+        <li><a href="/confused-sanae-46664363.jpg" target="_blank" class="text-purple-500">confused-sanae-46664363.jpg</a>: 
+            <a href="//pixiv.net/en/artworks/46664363">https://pixiv.net/en/artworks/46664363</a></li>
+        
+        <li><a href="/heart.svg" target="_blank" class="text-purple-500">heart.svg</a>: 
+            <a href="https://fontawesome.com/v5.15/icons/heart?style=solid">https://fontawesome.com/v5.15/icons/heart</a> (CC BY 4.0 International)</li>
+
+        <li><a href="/bars.svg" target="_blank" class="text-purple-500">bars.svg</a>: 
+            <a href="https://fontawesome.com/v5.15/icons/bars?style=solid">https://fontawesome.com/v5.15/icons/bars</a> (CC BY 4.0 International)</li>
+    </ul>
+
+    <br class="my-2">    
+    <p class="align-middle">
+        This site is create by <a href="//soopy.moe">Sophie</a>,
+        powered by <a href="//kit.svelte.dev">SvelteKit</a> and <a href="//tailwindcss.com">TailwindCSS</a>
+    </p>
+</div>
